dc919a8c3cf81b928c184044bfe76efd3518a78e
osasagain <osasogbokhun.com>
2023-07-10 18:48:09 +0100

    Added Sidebar

--- script.js
+++
@@ -1,9 +0,0 @@
-
-const hamburger = document.querySelector(".hamburger");
-const streamLink = document.querySelector(".stream-links");
-
-
-hamburger.addEventListener("click",  () => {
-    hamburger.classList.toggle("active");
-    streamLink.classList.toggle("active");
-});
